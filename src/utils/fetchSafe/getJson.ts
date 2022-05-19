0f6eed0f0bdf9ae2d46cec768a8dfe6f4bf318da
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-05-19 13:45:09 -0700

    fix: typing for node-fetch

---
+++ src/utils/fetchSafe/getJson.ts
@@ -0,0 +1,13 @@
+import type { ApiCall } from '../../ApiCall.js'
+import { logger } from '../logs.js'
+
+const getJson = async <T>(response: ApiCall<T>['response']): Promise<T|null> => {
+  const clone = response.clone()
+
+  return await clone.json().catch((err) => {
+    logger.error(err)
+    return null
+  }) as T | null
+}
+
+export { getJson }
