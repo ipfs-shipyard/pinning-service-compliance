6fd6b9848c5f307772f73d972b349fad61f6877c
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-06-07 12:42:50 -0700

    fix: Remove hardcoded delay for pinata

--- src/utils/getQueue.ts
+++ src/utils/getQueue.ts
@@ -1,18 +1,13 @@
-// import { TokenBucketLimiter } from '@dutu/rate-limiter'
 import PQueue from 'p-queue'
 
 type QueueInstance = InstanceType<typeof PQueue>
 const queues: Map<string, QueueInstance> = new Map()
 
-// const getQueue = (endpointUrl: string, options: ConstructorParameters<typeof PQueue>[0] = { concurrency: 1, intervalCap: 1, interval: 1000 }): QueueInstance => {
 const getQueue = (endpointUrl: string, options: ConstructorParameters<typeof PQueue>[0] = { concurrency: 1, intervalCap: 1, interval: 1000 }): PQueue => {
   if (queues.has(endpointUrl)) {
     return queues.get(endpointUrl) as QueueInstance
   }
-  let newQueueLimiter = new PQueue(options)
-  if (endpointUrl.includes('pinata')) {
-    newQueueLimiter = new PQueue({ ...options, interval: 5000 })
-  }
+  const newQueueLimiter = new PQueue(options)
 
   queues.set(endpointUrl, newQueueLimiter)
 
