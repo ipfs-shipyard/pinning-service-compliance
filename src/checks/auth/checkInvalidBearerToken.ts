4f45693a6368c80f641aaa7e426efa87e7755e15
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-06-16 11:17:36 -0700

    fix: expect 401 for unauthorized responses (#107)
    * fix: expect 401 for unauthorized responses
    
    * chore: Use

--- src/checks/auth/checkInvalidBearerToken.ts
+++ src/checks/auth/checkInvalidBearerToken.ts
@@ -1,6 +1,7 @@
 import { getJoiSchema } from '../../utils/getJoiSchema.js'
 import { ApiCall } from '../../ApiCall.js'
 import type { ServiceAndTokenPair } from '../../types.js'
+import { responseCode } from '../../expectations/index.js'
 
 const checkInvalidBearerToken = async (pair: ServiceAndTokenPair) => {
   const schema = await getJoiSchema('Failure')
@@ -11,10 +12,7 @@ const checkInvalidBearerToken = async (pair: ServiceAndTokenPair) => {
     schema,
     title: 'Request with invalid token'
   })
-    .expect({
-      title: 'Returns a 403',
-      fn: async ({ details }) => details.response.status === 403
-    })
+    .expect(responseCode(401))
     .runExpectations()
 }
 
