2b9d205344c4ba789ba62651018be593c3babdda
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2022-06-21 12:35:02 -0700

    Update getInlineCid.ts

--- src/utils/getInlineCid.ts
+++ src/utils/getInlineCid.ts
@@ -1,5 +1,5 @@
 import { CID, bytes } from 'multiformats'
-import * as raw from 'multiformats/codecs/raw'
+import { code, encode } from 'multiformats/codecs/raw'
 import { sha256 } from 'multiformats/hashes/sha2'
 import { logger } from './logs.js'
 
@@ -7,11 +7,10 @@ const { fromString } = bytes
 const getInlineCid = async (value: string = process.hrtime().toString()): Promise<string> => {
   const inlineUint8Array = fromString(value)
   try {
-    const bytes = raw.encode(inlineUint8Array) 
+    const bytes = encode(inlineUint8Array)
     const hash = await sha256.digest(bytes)
-    const cid = CID.create(1, raw.code, hash)
+    const cid = CID.create(1, code, hash)
     return cid.toString()
-
   } catch (err) {
     logger.error('Problem creating an inline CID', err)
     throw err
