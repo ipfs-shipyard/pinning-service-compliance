128c70c2ebd6fca9997dac3a7d3b855e1de95b2a
Enehizena Lawrence <lawstands@gmail.com>
2022-06-21 22:12:08 +0100

    Fix: PinResults, Pin Delete Response Code and CID Generation (#123)
    * based on the spec, delete response code is 202
    
    * name is optional, do the same for pin results
    
    * do not use identity codec for cid, most pinning service uses merkledag.Walk - identity codec is not registered
    
    * Update getInlineCid.ts
    
    Co-authored-by: Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>

--- src/utils/getInlineCid.ts
+++ src/utils/getInlineCid.ts
@@ -1,15 +1,16 @@
-import { identity } from 'multiformats/hashes/identity'
-import { bytes, CID } from 'multiformats'
-
+import { CID, bytes } from 'multiformats'
+import { code, encode } from 'multiformats/codecs/raw'
+import { sha256 } from 'multiformats/hashes/sha2'
 import { logger } from './logs.js'
 
 const { fromString } = bytes
 const getInlineCid = async (value: string = process.hrtime().toString()): Promise<string> => {
   const inlineUint8Array = fromString(value)
   try {
-    const inlineDateDigest = await identity.digest(inlineUint8Array)
-
-    return CID.createV1(identity.code, inlineDateDigest).toString()
+    const bytes = encode(inlineUint8Array)
+    const hash = await sha256.digest(bytes)
+    const cid = CID.create(1, code, hash)
+    return cid.toString()
   } catch (err) {
     logger.error('Problem creating an inline CID', err)
     throw err
