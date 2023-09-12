33781ab78ac76b7385c1a2318a6f72363338c9be
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-09-12 14:12:15 -0700

    fix: fetch no longer hangs during checkEmptyBearerToken with scaleway

--- src/clientFromServiceAndTokenPair.ts
+++ src/clientFromServiceAndTokenPair.ts
@@ -2,13 +2,25 @@ import { RemotePinningServiceClient, Configuration } from '@ipfs-shipyard/pinnin
 
 import { requestResponseLogger, RequestResponseLoggerOptions } from './middleware/requestReponseLogger.js'
 import type { ServiceAndTokenPair } from './types.js'
+import { logger } from './utils/logs.js'
 
 function clientFromServiceAndTokenPair ([endpointUrl, accessToken]: ServiceAndTokenPair, middleWareOptions?: Omit<RequestResponseLoggerOptions, 'pair'>): RemotePinningServiceClient {
   const requestResponseLoggerOptions: RequestResponseLoggerOptions = { ...middleWareOptions, pair: [endpointUrl, accessToken] }
+
   const config = new Configuration({
     endpointUrl,
     accessToken,
-    fetchApi: fetch,
+    fetchApi: async (url: RequestInfo | URL, init?: RequestInit) => {
+      const abortController = new AbortController()
+      const { signal } = abortController
+      setTimeout(() => abortController.abort(), 60000)
+      const result = await fetch(url, {
+        ...init,
+        signal: init?.signal ?? signal
+      })
+      logger.debug('fetchApi result obtained')
+      return result
+    },
     middleware: [
       requestResponseLogger(requestResponseLoggerOptions)
     ]
