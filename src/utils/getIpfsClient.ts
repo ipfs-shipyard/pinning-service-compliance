9c91abeb12599c5fbb03c43d1947e234158e467c
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-08-19 15:26:43 -0500

    deps: migrate go-ipfs -> kubo

--- src/utils/getIpfsClient.ts
+++ src/utils/getIpfsClient.ts
@@ -1,16 +1,11 @@
-import { join } from 'path'
-
-import { createController } from 'ipfsd-ctl'
-import * as createKuboClient from 'kubo-rpc-client'
-import type { HTTPClientExtraOptions } from 'kubo-rpc-client'
-import type { API } from 'ipfs-core-types/src/root'
+import { createNode } from 'ipfsd-ctl'
+import { create, KuboRPCClient } from 'kubo-rpc-client'
+import { path } from 'kubo'
 
 import { logger } from './logs.js'
 
-type InlineableClient = API<HTTPClientExtraOptions & {inline: boolean}>
-
 interface GetIpfsClientResponse {
-  client: InlineableClient
+  client: KuboRPCClient
   /**
    * A function that should be called when you're done using the client so cleanup can be performed.
    */
@@ -19,15 +14,16 @@ interface GetIpfsClientResponse {
 const getIpfsClient = async (): Promise<GetIpfsClientResponse> => {
   try {
     logger.debug('Attempting to use ipfsd-ctl to create a client')
-    const ipfsd = await createController({
-      kuboRpcModule: createKuboClient,
-      ipfsBin: join('node_modules', '.bin', 'ipfs'),
+    const ipfsd = await createNode({
+      type: 'kubo',
+      rpc: create,
+      bin: path(),
       test: true,
       disposable: true
     })
-    const client = ipfsd.api as InlineableClient
+    const client = ipfsd.api
 
-    if (await client.isOnline()) {
+    if (await client.isOnline() === true) {
       return {
         client,
         done: async () => { await ipfsd.stop() }
